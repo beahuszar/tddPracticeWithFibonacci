31694d0e8b9e47f74144712997926fb1d9b16d76
Huszar Beata <beata.huszar@adevinta.com>
2020-02-24 11:53:47 +0100

    Check if type is a function

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 var should = require('chai').should();
 var expect = require('chai').expect;
 var assert = require('chai').assert;
-var fibonacci = require('./fibonacci.js').getFibonacciByIndex();
+var fibonacci = require('./fibonacci.js').getFibonacciByIndex;
 
 describe('tests', () => {
   it('should exist', () => {
     expect(fibonacci).to.exist;
+  });
+
+  it('should be a function', () => {
+    fibonacci.should.be.a('function');
   });
 });
