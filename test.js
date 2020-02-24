47aa71ce93b9a710322151eb08f432a9577a879c
Huszar Beata <beata.huszar@adevinta.com>
2020-02-24 12:20:51 +0100

    Should have input & should return number

--- test.js
+++ test.js
@@ -11,4 +11,14 @@ describe('tests', () => {
   it('should be a function', () => {
     fibonacci.should.be.a('function');
   });
+
+  it('should return a number', () => {
+    fibonacci(1).should.be.a('number');
+  });
+
+  it('should have input', () => {
+    console.log(fibonacci());
+    fibonacci().to.throw('Input is not defined');
+    fibonacci(null).to.throw('Input should not be null');
+  });
 });
